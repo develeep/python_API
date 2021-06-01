45e4c66a711f5391dd7324896c0d5c4fb146f70e
shlee <83402978+develeep@users.noreply.github.com>
2021-06-01 23:54:42 +0900

    Search_trends_times
    시간에 따른 검색량 변화 찾기

---
+++ Search.py
@@ -0,0 +1,31 @@
+from pytrends.request import TrendReq
+import matplotlib.pyplot as plt
+import os
+
+# 검색 키워드, 검색 기간
+keyword = 'Galaxy Fold'
+period = 'today 1-m'    # 검색기간 최근 1개월
+
+
+# 구글 트렌드 접속
+trend_obj = TrendReq()
+trend_obj.build_payload(kw_list=[keyword],timeframe=period)
+
+# 시간에 따른 트렌드 변화
+trend_df = trend_obj.interest_over_time()
+print(trend_df.head())
+
+# 그래프 출력
+plt.style.use('ggplot')
+plt.figure(figsize=(14,5))
+trend_df['Galaxy Fold'].plot()
+plt.title('Google Trends over time', size = 15)
+plt.legend(labels=['Galaxy Fold'],loc='upper right')
+
+# 그래프 파일 저장
+cwd = os.getcwd()
+print(cwd)
+output_filepath = os.path.join(cwd,'output','google_trend_%s.png' %keyword)
+print(output_filepath)
+plt.savefig(output_filepath,dpi=300)
+plt.show()
